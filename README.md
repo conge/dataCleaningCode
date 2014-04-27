3931669f6dbfb2514f3e2b346c39705f3131c005
conge <congel@gmail.com>
2014-04-27 16:55:39 -0400

    code done, instruction done

--- README.md
+++ README.md
@@ -16,4 +16,38 @@ Create one R script called **run_analysis.R** that processes the raw data which
 4. Appropriately labels the data set with descriptive activity names. 
 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
+## Instructions
+
+### 1. Prepare directories and download data 
+    
+    mkdir ~/dataproject
+    cd ~/dataproject
+    mkdir tidyData script
+    wget https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+    unzip  getdata%2Fprojectfiles%2FUCI\ HAR\ Dataset.zip
+    ln -s  UCI\ HAR\ Dataset/ rawData
+
+### 2. download the **run_analysis.R** to the script folder created above.
+
+    cd ~/dataProject/script
+    wget https://github.com/conge/dataCleaningCode/blob/master/run_analysis.R
+
+### 3. Run the script in R.
+
+  you can run it under Linux/Unix by typing R at the prompt. 
+
+    R
+  
+Once in R enter
+
+    source('run_analysis.R')
+
+Wait for a few minutes and you'll see a file named **smartphone.cvs** in the folder ~/dataProject/tidyData
+
+## Codebook of smartphone.cvs
+
+
+  source('so.R')
+
+
 
--- run_analysis.R
+++ run_analysis.R
@@ -36,7 +36,8 @@ features_labels <- read.table(file='../rawData/features.txt')
 # Merge train and test data.
 subj <- rbind(subj_train,subj_test)
 X <- rbind(X_train,X_test)
-y <- rbind(y_train,y_test)
+activity <- rbind(y_train,y_test)
+
 body_acc_x <- rbind(body_acc_x_train,body_acc_x_test)
 body_acc_y <- rbind(body_acc_y_train,body_acc_y_test)
 body_acc_z <- rbind(body_acc_z_train,body_acc_z_test)
@@ -49,3 +50,57 @@ total_acc_z <- rbind(total_acc_z_train,total_acc_z_test)
 
 ## recollect memory
 rm(list=ls(pattern='_t'))
+
+## Extract Mean and standard deviation for measurements.
+body_acc_x_mean = rowMeans(body_acc_x)
+body_acc_x_sd = apply(data.matrix(body_acc_x),1,sd)
+body_acc_y_mean = rowMeans(body_acc_y)
+body_acc_y_sd = apply(data.matrix(body_acc_y),1,sd)
+body_acc_z_mean = rowMeans(body_acc_z)
+body_acc_z_sd = apply(data.matrix(body_acc_z),1,sd)
+
+body_gyro_x_mean = rowMeans(body_gyro_x)
+body_gyro_x_sd = apply(data.matrix(body_gyro_x),1,sd)
+body_gyro_y_mean = rowMeans(body_gyro_y)
+body_gyro_y_sd = apply(data.matrix(body_gyro_y),1,sd)
+body_gyro_z_mean = rowMeans(body_gyro_z)
+body_gyro_z_sd = apply(data.matrix(body_gyro_z),1,sd)
+
+total_acc_x_mean = rowMeans(total_acc_x)
+total_acc_x_sd = apply(data.matrix(total_acc_x),1,sd)
+total_acc_y_mean = rowMeans(total_acc_y)
+total_acc_y_sd = apply(data.matrix(total_acc_y),1,sd)
+total_acc_z_mean = rowMeans(total_acc_z)
+total_acc_z_sd = apply(data.matrix(total_acc_z),1,sd)
+
+
+df <- data.frame(body_acc_x_mean, body_acc_x_sd,
+                          body_acc_y_mean, body_acc_y_sd,
+                          body_acc_z_mean, body_acc_z_sd,
+                          body_gyro_x_mean, body_gyro_x_sd,
+                          body_gyro_y_mean, body_gyro_y_sd,
+                          body_gyro_z_mean, body_gyro_z_sd,
+                          total_acc_x_mean, total_acc_x_sd,
+                          total_acc_y_mean, total_acc_y_sd,
+                          total_acc_z_mean, total_acc_z_sd
+)
+
+rm(list=ls(pattern='acc'))
+rm(list=ls(pattern='gyro'))
+
+
+## activity names
+subj = as.factor(subj$V1)
+activity = as.factor(activity$V1)
+levels(activity) <-(list(WALKING=1,WALKING_UPSTAIRS=2,WALKING_DOWNSTAIRs=3,
+                  SITTING=4,STANDING=5,LAYING=6))
+
+data <- cbind(df,X)
+data$subj <- subj
+data$activity <- activity
+
+
+## average each variable in data for each activity and each subject.
+
+tidy <- aggregate(. ~ subj + activity, data = data, FUN= mean,na.rm=TRUE )
+write.csv(tidy,file='../tidyData/smartphone.csv')
